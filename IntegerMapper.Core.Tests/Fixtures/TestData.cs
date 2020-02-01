8ff9350c3dd4fb00e25d64d36a67dbd11d76dee8
Wikus Coetser <rjlcoetser@gmail.com>
2020-02-01 21:42:13 +1100

    Cleanup

--- IntegerMapper.Core.Tests/Fixtures/TestData.cs
+++ IntegerMapper.Core.Tests/Fixtures/TestData.cs
@@ -2,7 +2,10 @@
 {
     public class TestData<T>
     {
+#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
         public T Input { get; set; }
+#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+
         public ulong ExpectedOutput { get; set; }
     }
 }
