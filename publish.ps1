94cc668837c85edf3e1b190a446ff7037a0c8123
Wikus Coetser <rjlcoetser@gmail.com>
2020-03-09 18:09:00 +1100

    Fix publish script

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
 
-dotnet pack ./IntegerMapper.Core/IntegerMapper.Core.csproj --output .\nupkg --configuration Release
+dotnet pack ./Trs.IntegerMapper.Core/Trs.IntegerMapper.Core.csproj --output .\nupkg --configuration Release
 #dotnet nuget push "nupkg/IntegerMapper.Core.0.1.0.nupkg" --source "github"
